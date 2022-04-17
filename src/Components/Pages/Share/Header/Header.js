6fe6d7de32ba45db4558768e83aea4e50bc4ab7c
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-04-17 10:08:37 +0600

    header reload fix

--- src/Components/Pages/Share/Header/Header.js
+++ src/Components/Pages/Share/Header/Header.js
@@ -7,16 +7,16 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#home">Gym-Trainer</Navbar.Brand>
+  <Navbar.Brand as={Link} to="/home">Gym-Trainer</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="m-auto">
-      <Nav.Link href="service">Services</Nav.Link>
-      <Nav.Link href="/blog">Blog</Nav.Link>
+      <Nav.Link as={Link} to="service">Services</Nav.Link>
+      <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
     </Nav>
     <Nav>
       <Nav.Link as={Link} to="/about">About</Nav.Link>
-      <Nav.Link  href="#memes">
+      <Nav.Link as={Link} to="/login">
         LogIn
       </Nav.Link>
     </Nav>
