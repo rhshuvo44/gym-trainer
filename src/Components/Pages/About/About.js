2c29acfe472d944d6036b3cf9401810a56611e20
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-04-18 16:57:46 +0600

    about pages

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import mePic from '../../Images/me.jpg';
 
 const About = () => {
     return (
         <div className='container py-5'>
-            <h1>About</h1>
+            <h1 className='mb-5'>About Me</h1>
+            <div className="row">
+                <div className="col-md-6">
+<img src={mePic} alt="" />
+                </div>
+                <div className="col-md-6">
+        <h3>Ripon Hossain Shuvo</h3>
+        <p className='p-5'>I'm a Front End Web Developer. An enthusiast of technology always enjoys coding, likes to solve programming problems. I am interested in thinking/developing web-related modules or functionality and always try to find new ideas. I have eight months of experience in Web Design and Development. I live in Dhaka, Bangladesh. I am currently studying at Chandpur Polytechnic Institute, in Computer Technology. In the tech community, I try to help people by sharing my knowledge. I love to read tech-related articles & play video games during my free time</p>
+                </div>
+            </div>
         </div>
     );
 };
