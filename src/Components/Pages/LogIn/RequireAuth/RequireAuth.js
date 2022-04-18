9c0b7e74fbe1562c18068ad2784562ecad4a836c
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-04-18 11:31:07 +0600

    reload problem fix

--- src/Components/Pages/LogIn/RequireAuth/RequireAuth.js
+++ src/Components/Pages/LogIn/RequireAuth/RequireAuth.js
@@ -4,9 +4,15 @@ import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../../../firebase.init';
 
 const RequireAuth = ({children}) => {
-    const [user] = useAuthState(auth);
+    const [user,loading] = useAuthState(auth);
   const location = useLocation();
-
+if (loading) {
+  return (
+    <div className='py-5'>
+      <p>Initialising User...</p>
+    </div>
+  );
+}
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
